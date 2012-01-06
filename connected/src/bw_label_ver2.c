4b3bbce77a426ed7dbd3a4791d2d97d511e4ed2c
harcourt <harcourt@localhost>
2012-01-05 22:39:11 -0500

    Updated to fix pointer and memory issues using multidimensional arrays.
     Enter commit message.  Lines beginning with 'HG:' are removed.

--- connected/src/bw_label_ver2.c
+++ connected/src/bw_label_ver2.c
@@ -55,43 +55,50 @@ static void Union(int x, int y) {
     link(find_set(x), find_set(y));
 }
 
-static int **label(int *image[], int rows, int cols) {
+static void init_mem(int *image, int val, int n) {
+     for (int *p = image; p < (image + n); p++)
+    	 *p = val;
+}
+
+static int *label(int image[][6], int rows, int cols) {
 
 	int next_label = 1;
     int neighbors[4];
 	int nc = 0; /* neighbor count */
 
-    int **labels = (int **) malloc(rows*cols);
+    int *labels = (int *) malloc(rows * cols * sizeof(int));
 
-	memset(labels, BACKGROUND, rows*cols);
+
+	/*memset(labels, BACKGROUND, rows*cols); */
+    init_mem(labels, BACKGROUND, rows*cols);
 
 	for (int r = 1; r < rows - 1; r++) {
-		memset(&neighbors, INT_MAX, 4);
+		init_mem(&neighbors[0], INT_MAX, 4);
 		nc = 0;
 
 		for (int c = 1; c < cols - 1; c++) {
 			if (image[r][c] == BACKGROUND) continue;
 
-			if (labels[r][c-1] != BACKGROUND)   /* west */
-				neighbors[nc++] = labels[r][c-1];
+			if (labels[r*cols + c-1] != BACKGROUND)   /* west */
+				neighbors[nc++] = labels[r*cols + c-1];
 
-			if (labels[r-1][c-1] != BACKGROUND) /* north-west */
-				neighbors[nc++] = labels[r-1][c-1];
+			if (labels[(r-1)*cols + (c-1)] != BACKGROUND) /* north-west */
+				neighbors[nc++] = labels[(r-1)*cols + (c-1)];
 
-			if (labels[r-1][c] != BACKGROUND)   /* north */
-				neighbors[nc++] = labels[r-1][c];
+			if (labels[(r-1)*cols + c] != BACKGROUND)   /* north */
+				neighbors[nc++] = labels[(r-1)*cols + c];
 
-			if (labels[r-1][c+1] != BACKGROUND) /* north-east */
-				neighbors[nc++] = labels[r-1][c+1];
+			if (labels[(r-1)*cols + (c+1)] != BACKGROUND) /* north-east */
+				neighbors[nc++] = labels[(r-1)*cols + (c+1)];
 
 			if (nc == 0) {
 				make_set(next_label);
-				labels[r][c] = next_label;
+				labels[r*cols + c] = next_label;
 				next_label++;
 			}
 			else {
-                labels[r][c] = min4(neighbors[0],neighbors[1],
-                		            neighbors[2],neighbors[3]);
+                labels[r*cols + c] = min4(neighbors[0],neighbors[1],
+                		                  neighbors[2],neighbors[3]);
 
                 /* make sure all neighbors are in the same set */
                 for (int i = 0; i < nc; i++)
@@ -107,7 +114,7 @@ static int **label(int *image[], int rows, int cols) {
 		for (int r = 1; r < rows - 1; r++)
 			for (int c = 1; c < cols - 1; c++)
 				if (image[r][c] != BACKGROUND)
-					labels[r][c] = find_set(labels[r][c]);
+					labels[r*cols+c] = find_set(labels[r*cols+c]);
 	} /* row */
 
 	return labels;
